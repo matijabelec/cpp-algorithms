de2677ba4b51c025e0db56ef0409d04fb39e7167
matijabelec <mbelec@foi.hr>
2015-06-22 10:37:54 +0200

    update

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "cpp-algorithms" << endl;
+    
+	return 0;
+}
+
+// matijabelec 2015
--- sort/sort-algorithms1.cpp
+++ sort/sort-algorithms1.cpp
@@ -20,7 +20,7 @@ void Izbornik();
 int main() {
 	Izbornik();
 	
-	system("pause");
+	int stop; cin>>stop;
 	return 0;
 }
 
