29b91aa4f7f728b1eccdd5d2062d187dfb1bed5c
matijabelec <mbelec@foi.hr>
2015-06-22 17:42:39 +0200

    added 2 files

---
+++ prog1-examples/labs/1415/15051815001.cpp
@@ -0,0 +1,62 @@
+/*******************************************************************************
+    Napravite program koji rješava sljedeći problem:
+    
+    Ulaz: Prirodni broj N i N decimalnih brojeva.
+    Izlaz: U polju dimenzije N sortirati silazno brojeve algoritmom sortiranja 
+           umetanjem.
+
+    --
+    Ulazni testni podaci:
+    10
+    461
+    641
+    727
+    461
+    33
+    7528
+    335
+    361
+    75
+    46
+
+    Izlazni testni podaci:
+    7528
+    727
+    641
+    461
+    461
+    361
+    335
+    75
+    46
+    33
+
+ *******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    do
+        cin >> N;
+    while(N<1);
+    
+    float* polje = new float[N];
+    for(int i=0; i<N; i++)
+        cin >> polje[i];
+    
+    for(int i=1; i<N; i++) {
+		int j = i-1;
+		int pom = polje[i];
+		while(j>=0 && polje[j]<pom)
+			polje[j+1] = polje[j--];
+		polje[j+1] = pom;
+	}
+    
+    for(int i=0; i<N; i++)
+        cout << polje[i] << endl;
+    delete[] polje;
+    
+    return 0;
+}
---
+++ prog1-examples/labs/1415/15051815004.cpp
@@ -0,0 +1,66 @@
+/*******************************************************************************
+    Napravite program koji rješava sljedeći problem:
+    
+    Ulaz: Prirodni broj N i N decimalnih brojeva.
+    Izlaz: U polju dimenzije N sortirati brojeve algoritmom mjehuricastog 
+           sortiranja.
+
+    --
+    Ulazni testni podaci:
+    10
+    461
+    641
+    727
+    461
+    33
+    7528
+    335
+    361
+    75
+    46
+
+    Izlazni testni podaci:
+    33
+    46
+    75
+    335
+    361
+    461
+    461
+    641
+    727
+    7528
+
+ *******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    do
+        cin >> N;
+    while(N<1);
+    
+    float* polje = new float[N];
+    for(int i=0; i<N; i++)
+        cin >> polje[i];
+    
+    bool zam = true;
+    for(int i=N-1; i>0 && zam; i--) {
+        zam = false;
+        for(int j=0; j<i; j++)
+            if(polje[j]>polje[j+1]) {
+                int pom = polje[j];
+                polje[j] = polje[j+1];
+                polje[j+1] = pom;
+                zam = true;
+            }
+    }
+    
+    for(int i=0; i<N; i++)
+        cout << polje[i] << endl;
+    delete[] polje;
+    
+    return 0;
+}
