9edb4176f362636a74d6a4726a19f86142b9c39f
matijabelec <mbelec@foi.hr>
2015-06-24 16:39:25 +0200

    added new file (similar to 15062400012)

---
+++ prog1-examples/labs/1415/15062400022.cpp
@@ -0,0 +1,117 @@
+/*******************************************************************************
+    Napravite program koji rješava sljedeći problem:
+
+    Ulaz: Prirodni broj N, N cijelih brojeva i I, 0<=I<=49
+    Izlaz: Napraviti hash-tablicu s 50 pretinaca i u nju upisati N
+           pseudoslučajnih brojeva. Ispisati Najmanji broj u I-tom pretincu 
+           tablice.
+
+    --
+    Ulazni testni podaci:
+    40
+    717
+    345
+    332
+    5401
+    2332
+    763
+    2626
+    4626
+    4527
+    3433
+    2337
+    426
+    3355
+    62722
+    3543
+    2429
+    33549
+    3533
+    3246
+    7557
+    7537
+    444
+    356
+    272
+    6427
+    4442
+    9234
+    3561
+    6417
+    3993
+    3161
+    6531
+    6417
+    6441
+    46177
+    4161
+    4617
+    483
+    8353
+    22023
+    22
+
+    Izlazni testni podaci:
+    272
+
+ *******************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#define BR_PRETINACA 50
+
+struct elem {
+    int vr;
+    elem* sl;
+}** ht;
+
+int main () {
+    int N;
+    int I;
+
+    do {
+        cin >> N;
+    } while(N<1);
+
+    ht = new elem*[BR_PRETINACA];
+    for(int i=0; i<BR_PRETINACA; i++) {
+        ht[i] = NULL;
+    }
+
+    for(int i=0; i<N; i++) {
+        elem* novi = new elem;
+        cin >> novi->vr;
+        novi->sl = ht[novi->vr%BR_PRETINACA];
+        ht[novi->vr%BR_PRETINACA] = novi;
+    }
+
+    do {
+        cin >> I;
+    } while(I<0 || I>=BR_PRETINACA);
+
+    int mini;
+    bool nema = true;
+    for(elem* tr=ht[I]; tr; tr=tr->sl)
+        if(nema) {
+            mini = tr->vr;
+            nema = false;
+        } else if(tr->vr < mini)
+            mini = tr->vr;
+
+    if(nema)
+        cout << "pretinac je prazan" << endl;
+    else
+        cout << mini << endl;
+
+    for(int i=0; i<BR_PRETINACA; i++)
+        for(elem* tr=ht[i]; tr; ) {
+            elem* br = tr;
+            tr = tr->sl;
+            delete br;
+        }
+    delete[] ht;
+
+    return 0;
+}
