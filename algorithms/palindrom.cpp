235152afd5c7ef70cc2ed15681d773665e100f4d
matijabelec <mbelec@foi.hr>
2015-06-25 20:59:59 +0200

    updated

--- algorithms/palindrom.cpp
+++ algorithms/palindrom.cpp
@@ -3,10 +3,25 @@
 #include <cstring>
 using namespace std;
 
+//
+// rekurzivni pristup
+// r je string
+// k je broj znakova od r umanjen za 1
 bool palindrom(string& r, int k, int i=0) {
     if(i>k-i) return true;
     if(r[i] != r[k-i]) return false;
     return palindrom(r, k, i+1);
+}
+
+//
+// iterativni pristup
+// r je string
+// n je broj znakova od r
+bool palindrom2(string& r, int n) {
+    for(int i=0; i<n; i++)
+        if(r[i] != r[n-i-1])
+            return false;
+    return true;
 }
 
 int main() {
@@ -15,7 +30,9 @@ int main() {
 
     for(int i=0; i<n; i++)
         cout << rijeci[i] << " => "
-             << palindrom(rijeci[i], rijeci[i].length()-1) << endl;
+             << palindrom(rijeci[i], rijeci[i].length()-1) << " | "
+             << palindrom2(rijeci[i], rijeci[i].length() )
+             << endl;
 
     return 0;
 }
