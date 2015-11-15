dc5acee2f5def64e27ca1e6832b43b838b10cb07
matijabelec <mbelec@foi.hr>
2015-11-15 20:17:34 +0100

    updated counting sort algorithm

--- sort/counting_sort.cpp
+++ sort/counting_sort.cpp
@@ -2,16 +2,61 @@
 using namespace std;
 
 // sortiranje prebrojavanjem
-void CountSort(int polje[], int n) {
-    //TODO: implement
+void CountSort(int polje[], int n, int raspon=10000) {
+	
+	// pripremi pomocne varijable
+	int i, stari_brojac, ukupno=0;
+	
+	// kopiraj sve elemente iz polja u pomocno polje
+	int* polje_original = new int[n];
+	for(i=0; i<n; i++) {
+		polje_original[i] = polje[i];
+	}
+	
+	// pripremi polje za brojace (prvo se broji koliko 
+	// se puta svaki element pojavljuje u polju, a nakon
+	// toga se brojaci posloze da pokazuju indekse (lokacije)
+	// u sortiranom polju na koje se pojedini element mora
+	// postaviti)
+	int* brojac = new int[raspon];
+	for(i=0; i<raspon; i++) brojac[i]=0;
+	
+	// izracunavanje broja ponavljanja za svakog elementa
+	for(i=0; i<n; i++) {
+		brojac[polje[i] ]++;
+	}
+	
+	// pretvaranje brojaca u indekse na kojima ce se elementi
+	// zapisatu u sortiranom polju
+	for(i=0; i<raspon; i++) {
+		stari_brojac = brojac[i];
+		brojac[i] = ukupno;
+		ukupno += stari_brojac;
+	}
+	
+	// kopiranje elemenata iz pomocnog polja u sortirano
+	// pri cemu se pazi na redoslijed elementa (kao pomoc za
+	// redoslijed koristi se polje 'brojac')
+	for(int i=0; i<n; i++) {
+		polje[brojac[polje_original[i] ] ] = polje_original[i];
+		brojac[polje_original[i] ] += 1;
+	}
+	
+	// oslobodi zauzetu memoriju
+    delete[] brojac;
+    delete[] polje_original;
 }
 
 int main() {
+	
+	// pripremi testno polje
     int polje[]={12,53,35,36,3,2,5};
     int n = sizeof(polje)/sizeof(int);
     
+    // sortiraj polje
     CountSort(polje, n);
     
+    // prikazi sortirano polje
     for(int i=0; i<n; i++)
         cout << polje[i] << " ";
     cout << endl;
@@ -21,7 +66,7 @@ int main() {
 
 
 /**************************************************************************************
- * from wiki - todo: translate to c/c++ 
+ * from wiki
  **************************************************************************************
 # variables:
 #    input -- the array of items to be sorted; key(x) returns the key for item x
