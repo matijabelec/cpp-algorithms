ebc8322cdba1d0fea611c64ee4ac5d7590604a9c
matijabelec <mbelec@foi.hr>
2015-11-21 14:45:02 +0100

    added tutorials folder; added simple intro-tovariables

---
+++ tutorials/01-variables/README.md
@@ -0,0 +1,6 @@
+# 01 - Variables
+
+Variables are named memory locations. We use them to store some data.
+
+How data is stored and read, are described by a type of variable. Type of 
+variable specifies variables size in memory.
---
+++ tutorials/01-variables/intro-to-variables.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   intro-to-variables.cpp
+ * Author: matija
+ *
+ * Created on November 21, 2015, 2:36 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // var = variable; an memory location used to store data
+    // int = type of variable; it specifies which data and how is it stored in
+    //       selected memory location
+    int var;
+    
+    // user input (from standard console) we store into the variable
+    cin >> variable;
+    
+    // now we outputs (to standard console) stored user input
+    cout << variable << endl;
+    
+    return 0;
+}
